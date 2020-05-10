372fa5618027a534c59fc5c6731f12edb863983b
Sefa Melih Dal <sefad@korozo.com.tr>
2020-05-10 16:39:20 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetbyId(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+
+    }
+}
